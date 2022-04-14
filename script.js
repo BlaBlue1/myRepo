0204647b66cfcc2e1d0a847d65e0c2c8e4f75b69
RuthWudu <ruthwudu576@gmail.com>
2022-04-14 05:08:33 -0400

    add files

---
+++ script.js
@@ -0,0 +1,160 @@
+//Code Foundations exercises
+
+
+//  1. Declare a variable and assign a String value to it. This variable should be able to be updated with a new value.
+// let name = 'Ruth';
+// Ruth = 'Is a person';
+// Ruth = 'Is a student';
+// console.log(Ruth);
+
+
+
+
+//  2. Declare a variable and assign a number value to it. This variable should not be able to be updated with a new value.
+// const x = 19;
+// console.log(x);
+
+
+//  3. Declare a variable and assign a boolean value to it. You may use any of the three keywords to declare this variable.
+// let greeting = true;
+// console.log(!greeting);
+// console.log(greeting);
+
+
+//  4. Consider the following code. What will be the value of z?
+// let x = 5;
+// let y = 10;
+// let z = x + y;
+// console.log(z);
+
+
+//  5. Consider the following code. What will be the value of z?
+// let x = '10';
+// let y = '15';
+// let z = x + y;
+// console.log(z);
+
+
+//  6. Consider the following code. What will be the value of z?
+// let x = 20;
+// let y = '30';
+// let z = x + y;
+// console.log(z);
+
+
+//  7. Consider the following code. What will be the value of z?
+// let x = 10;
+// let y = x * 2;
+// let z = y - 5;
+// console.log(z);
+
+
+//  8. Consider the following code. What will z evaluate to?
+// let x = 10;
+// let y = 15;
+// let z = x > y;
+// console.log(z);
+
+
+//  9. Consider the following code. What will z evaluate to?
+// let x = 10;
+// let y = '10';
+// let z = x == y;
+// console.log(z);
+
+
+//  10. Consider the following code. What will z evaluate to?
+// let x = 15;
+// let y = '15';
+// let z = x === y;
+// console.log(z);
+
+
+//  11. Consider the following code. What will z evaluate to?
+// let x = 20;
+// let y = 21;
+// let z = x = y;
+// console.log(z);
+
+
+//  12. Write a function that takes in two parameters and returns the result of adding the values of the two parameters.
+// let pet = "dog ";
+// let breed = "Husky";
+// let myPet = pet + breed;
+// console.log(myPet);
+
+//  13. Write a function that takes in one parameter and returns the value of that parameter.
+// function bark(){
+// for(let i = 0; i<1; i++)
+//    console.log("Ruff");
+// } 
+// bark();
+
+    
+
+//  14. What would the value of x need to be in order for our function to output 'Yes'.
+// function myFunc(x) {
+//     if(x != 'name') {
+//         return 'Yes';
+//     }
+//     else {
+//         return 'No';
+//     }
+// }
+// console.log(myFunc());
+
+
+//  15. How many times will the word "Hello" print to the console?
+// function sayHello() {
+//     for(let i = 0; i < 5; i++) {
+//         console.log("Hello");
+//     }
+// }
+// sayHello();
+
+
+//  16. What will be the output of the function below?
+// function count() {
+//     for(let i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }
+// count();
+
+
+//  17. What will be the output of the function below?
+// function count() {
+//     for(let i = 0; i < 4; i++) {
+//         console.log(i);
+//     }
+// }
+// count();
+
+
+//  18. What will the variable 'item' evaluate to?
+// let myList = ["one", "two", "three"];
+// let item = myList[1];
+// console.log(item);
+
+
+//  19. What values do we need to input for x and y in order for our code to return the String "Hello World"?
+// let myList = ["Say", "Hello", "To", "Someone", "or", "to", "the", "World"];
+// let x = [1];
+// let y = myList.length-1;
+// console.log(myList[x] + " " + myList[y]);
+
+// function run(){
+//    for (let i=0; i<4; i++){
+//       console.log(i);
+//    }
+// }
+// run();
+
+
+
+
+
+
+
+
+
